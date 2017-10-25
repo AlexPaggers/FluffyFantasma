33857199de0862da026cc024712aeca6da38bde7
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-10-25 17:52:24 +0100

    procedural level design

---
+++ Assets/Rooms/Scripts/Room.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Room : MonoBehaviour
+{
+    public bool activeInLevel = false;
+    public bool Active
+    {
+        get
+        {
+            return activeInLevel;
+        }
+        set
+        {
+            activeInLevel = value;
+        }
+    }
+}
---
+++ Assets/Rooms/Scripts/RoomLists.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLists : MonoBehaviour
+{
+    public List<GameObject> safeRooms = new List<GameObject>();
+    public List<GameObject> rooms01   = new List<GameObject>();
+    public List<GameObject> rooms02   = new List<GameObject>();
+    public List<GameObject> rooms03   = new List<GameObject>();
+    public List<GameObject> rooms04   = new List<GameObject>();
+    public List<GameObject> bossRooms = new List<GameObject>();
+}
