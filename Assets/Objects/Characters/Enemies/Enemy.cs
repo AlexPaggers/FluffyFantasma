3f0326de6ce59faa2f87cf8895cf72e46028a437
jackalyn m <kaos@thegame.com>
2017-10-30 17:03:46 +0000

    some work towards enemies, not sure what's useful here. also 1 -ups, (still need linking to gamedata when that is ready)

---
+++ Assets/Objects/Characters/Enemies/Enemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+	private AudioSource source { get { return this.GetComponent<AudioSource>(); } }
+
+	public GameObject coin;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Bullet")
+		{
+			SpawnDrops();
+			DeathAudio();
+			StartCoroutine(DestroyOnceEffectsHaveFinished());
+
+		}
+	}
+
+	private void SpawnDrops()
+	{
+		int total = Random.Range (2, 12);
+		for (int i = 0; i < total; i++) 
+		{
+			Instantiate (coin);
+
+		}
+
+	}
+
+
+	private void DeathAudio()
+	{
+		//	if (!source.isPlaying)
+		//		source.Play();
+
+		//re-add when audio is found
+	}
+
+
+	private IEnumerator DestroyOnceEffectsHaveFinished()
+	{
+		while (source.isPlaying)
+		{
+			yield return false;
+		}
+		Destroy(this.gameObject);
+		yield return true;
+	}
+}
--- Assets/Objects/TrackPlayer.cs
+++ Assets/Objects/TrackPlayer.cs
@@ -8,7 +8,7 @@ public class TrackPlayer : MonoBehaviour {
 	public GameObject target;
 	//public Transform target;
 	public float moveSpeed = 3;
-	public float rotSpeed = 3;
+	//public float rotSpeed = 3;
 	public float lookDist = 20; //enemies can look at the player (for shooting at them)
 	public float moveDist = 10; //objects can move to the player
 	public float stopDist = 0; //objects stop a certain distance from the player
