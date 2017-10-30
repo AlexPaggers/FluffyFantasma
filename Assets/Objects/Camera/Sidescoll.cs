0037f0af47947d04d244aec7bc0988c6d7b7824e
jackalyn m <kaos@thegame.com>
2017-10-30 14:14:00 +0000

    Added a script to move the camera, it's a bit slow atm. needs to know whether there is a boss to stop so it isn't added to the prefab until then

---
+++ Assets/Objects/Camera/Sidescoll.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sidescoll : MonoBehaviour {
+
+
+
+	public GameObject target;
+	public float speed = 2;
+	public float adjust = 0;
+	private Transform current;
+	private bool bossRoom = false;
+	// Use this for initialization
+	void Start ()
+	{
+		target = GameObject.FindWithTag("Player"); //target the player
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		//set whether in a boss room or not based on gamedata :)
+
+		if (!bossRoom) {
+			if (target.transform.position.x > current.position.x + adjust) {
+				//move towards to the right
+				current.position += current.right * speed * Time.deltaTime;
+			}
+		}
+	}
+
+
+	void Awake()
+	{
+		current = transform;
+	}
+}
