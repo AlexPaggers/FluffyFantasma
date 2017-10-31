20eb723203cac66da3abf63f13f2a19e69d680ad
g2-pelns <Gundars2.Pelns@live.uwe.ac.uk>
2017-10-31 13:28:51 +0000

    Yaya

---
+++ Assets/Objects/Power-Ups/PowerUpManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PUType
+{
+    SPEED_IN,
+    TRIPLE,
+    FOLLOW
+}
+
+public class PowerUpManager : MonoBehaviour {
+
+    public PUType pUpType;
+	// Use this for initialization
+	void Start () {
+        switch (pUpType)
+        {
+            case PUType.SPEED_IN:
+                //Set Sprite as SPEED INCREASE
+                break;
+            case PUType.TRIPLE:
+                //Set Sprite as TRIPLE SHOOT
+                break;
+            case PUType.FOLLOW:
+                //Set Sprite as FOLLOW
+                break;
+            default:
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            switch (pUpType)
+            {
+                case PUType.SPEED_IN:
+                    //Increase SPEED
+                    break;
+                case PUType.TRIPLE:
+                    //Create SHOTGUN
+                    break;
+                case PUType.FOLLOW:
+                    //Bullets FOLLOW
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
