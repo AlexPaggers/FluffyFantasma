3ed5d631ace49555642e8b3b3613d417d32d31bf
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-11-01 02:15:32 +0000

    camera move code added needs testing

--- Assets/Objects/Camera/Sidescoll.cs
+++ Assets/Objects/Camera/Sidescoll.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sidescoll : MonoBehaviour {
-
-
-
+public class Sidescoll : MonoBehaviour
+{
 	public GameObject target;
 	public float speed = 2;
 	public float adjust = 0;
 	private Transform current;
 	public bool bossRoom = false;
+
+
+    private GameObject currentRoom;
+    private GameObject nextRoom;
+    public GameObject roomOne;
+    public GameObject roomTwo;
+
+
+
 	// Use this for initialization
 	void Start ()
 	{
         //target the player
+        currentRoom = roomOne;
         
     }
 
@@ -30,23 +38,57 @@ public class Sidescoll : MonoBehaviour {
     // Update is called once per frame
     void LateUpdate () 
 	{
-		//set whether in a boss room or not based on gamedata :)
-
-		if (!bossRoom)
+        if (TrapDoor.TrapDoorHit)
         {
-			if (target.transform.position.x > current.position.x + adjust) 
+            if(currentRoom == roomOne)
             {
-                var newPosition = Vector3.Lerp(current.position, target.transform.position, speed * Time.deltaTime);
-                current.position = new Vector3(newPosition.x, current.position.y, current.position.z);
-				//move towards to the right
-				//current.position += current.right * speed * Time.deltaTime;
-			}
-		}
-	}
+                nextRoom = roomTwo;
+            }
+            else
+            {
+                nextRoom = roomOne;
+            }
+            StartCoroutine(MoveToRoom());
+        }
+
+
+
+
+        //set whether in a boss room or not based on gamedata :)
+        //if (TrapDoor.TrapDoorHit)
+        //{
+        //	if (target.transform.position.x > current.position.x + adjust) 
+        //    {
+        //        var newPosition = Vector3.Lerp(current.position, target.transform.position, speed * Time.deltaTime);
+        //        current.position = new Vector3(newPosition.x, current.position.y, current.position.z);
+        //		//move towards to the right
+        //		//current.position += current.right * speed * Time.deltaTime;
+        //	}
+        //}
+    }
 
 
 	void Awake()
 	{
 		current = transform;
 	}
+
+
+    private IEnumerator MoveToRoom()
+    {
+        while(true)
+        {
+            var newPosition = Vector3.Lerp(current.position, nextRoom.transform.position, speed * Time.deltaTime);
+            current.position = new Vector3(newPosition.x, current.position.y, current.position.z);
+
+            if(Vector2.Distance(transform.position, nextRoom.transform.position) < 0.5f)
+            {
+                break;
+            }
+
+            yield return false;
+        }
+        currentRoom = nextRoom;
+        yield return true;
+    }
 }
---
+++ Assets/_NEW PROJECT/Room/TrapDoor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TrapDoor : MonoBehaviour
+{
+    private static bool trapDoorHit;
+    public static bool TrapDoorHit
+    {
+        get
+        {
+            return trapDoorHit;
+        }
+        
+        internal set
+        {
+            trapDoorHit = value;
+        }
+    }
+
+    private void Start()
+    {
+        TrapDoorHit = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            TrapDoorHit = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            TrapDoorHit = false;
+        }
+    }
+	
+}
